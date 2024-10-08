d8c6fc60b70d567c0d30d74379854c11ddd7c139
DeviPusuluri <DeviPusuluri@gmail.com>
2024-10-08 14:21:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My E-Commerce App!</h1>
+        <p>This is a custom React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
